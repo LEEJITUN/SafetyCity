c1aec5543b8d3b3dd7376ac8a831fb5a451b7e0f
SangHyup <jsh170303@naver.com>
2021-12-24 09:21:43 +0900

    수정

--- src/main/resources/static/assets/demo/chart-cctv.js
+++ src/main/resources/static/assets/demo/chart-cctv.js
@@ -14,40 +14,8 @@ var myLineChart = new Chart(ctx, {
         label: "Revenue",
         backgroundColor: "rgba(2,117,216,1)",
         borderColor: "rgba(2,117,216,1)",
-        data: [4215, 5312, 6251, 7841, 9821, 14984],
+        data: [4215, 5312, 6251, 7841, 9821],
       },
     ],
-  },
-  options: {
-    scales: {
-      xAxes: [
-        {
-          time: {
-            unit: "month",
-          },
-          gridLines: {
-            display: false,
-          },
-          ticks: {
-            maxTicksLimit: 6,
-          },
-        },
-      ],
-      yAxes: [
-        {
-          ticks: {
-            min: 0,
-            max: 15000,
-            maxTicksLimit: 5,
-          },
-          gridLines: {
-            display: true,
-          },
-        },
-      ],
-    },
-    legend: {
-      display: false,
-    },
   },
 });
