7ca24b450aa9c4b4d9de8852c85b476b2a5ce88f
SangHyup <jsh170303@naver.com>
2021-12-30 15:39:26 +0900

    시간대별 페이지

--- src/main/java/com/Bigdata/safetycity/repository/CrimeRepository.java
+++ src/main/java/com/Bigdata/safetycity/repository/CrimeRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CrimeRepository extends JpaRepository<Crime, Long> {
-    List<Crime> findCrimesByTime(String time);
+    List<Crime> findCrimesByTimeOrderByCountDesc(String time);
+    List<Crime> findTop5ByTimeOrderByCountDesc(String time);
     List<Crime> findCrimesByName(String name);
 }
--- src/main/resources/static/demo/chart-area-demo.js
+++ src/main/resources/static/demo/chart-area-demo.js
@@ -6,12 +6,39 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 let myAreachart = document.getElementById("myAreaChart");
 let currtime = document.querySelector('.curr-time');
 
-let currentTime = new Date(Date.now()).fo
-currtime.innerHTML = currentTime.toString();
+let currentTime = new Date();
+
+
+currtime.innerHTML = '현재 시각은 ' +
+    currentTime.getHours() + ' 시 ' +
+    currentTime.getMinutes() + ' 분 ' +
+    currentTime.getSeconds() + ' 초 입니다.';
 
 function random_rgba() {
-    var o = Math.round, r = Math.random, s = 255;
+    var o = Math.round, r = Math.random, s = 200
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+function getTimeforDb(){
+    let currHour = currentTime.getHours();
+
+    if(currHour < 3){
+        return "00:00~02:59";
+    }else if(currHour < 6){
+        return "03:00~05:59";
+    }else if(currHour < 9){
+        return "06:00~08:59";
+    }else if(currHour < 12){
+        return "09:00~11:59";
+    }else if(currHour < 15){
+        return "12:00~14:59";
+    }else if(currHour < 18){
+        return "15:00~17:59";
+    }else if(currHour < 21){
+        return "18:00~20:59";
+    }else if(currHour < 24){
+        return "21:00~23:59";
+    }
 }
 
 fetch("/api/crime")
@@ -49,6 +76,44 @@ fetch("/api/crime")
             },
         });
     });
+
+let myBarChart = document.getElementById("myBarChart");
+let labels = []
+let datas = []
+
+fetch("/api/crime/time/" + getTimeforDb())
+    .then((res) => res.json())
+    .then((content) => {
+        console.log(content);
+        content.map((data) => {
+            labels.push(data.name);
+            datas.push(data.count);
+        })
+    })
+    .then(() => {
+        if(myBarChart){
+            new Chart(myBarChart, {
+                type: "bar",
+                data: {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: "위험지수",
+                            backgroundColor: "rgba(200, 50, 50, 1)",
+                            borderColor: "rgba(200, 50, 50, 1)",
+                            data: datas,
+                        },
+                    ],
+                },
+            });
+        }
+    });
+
+let currtimetext = document.getElementById('curr-time');
+
+currtimetext.innerHTML = '현재 시간 ' + currentTime.getHours() + ' : '
+    + currentTime.getMinutes() + ' 의 위험 지수는!'
+
 
 
 
