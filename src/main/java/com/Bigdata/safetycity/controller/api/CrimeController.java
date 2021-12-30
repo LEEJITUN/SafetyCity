dd42c681afcca389b68b036ed4e58545799d4e1a
SangHyup <jsh170303@naver.com>
2021-12-30 12:55:51 +0900

    시간대 그래프

--- src/main/java/com/Bigdata/safetycity/controller/api/CrimeController.java
+++ src/main/java/com/Bigdata/safetycity/controller/api/CrimeController.java
@@ -1,19 +1,15 @@
 package com.Bigdata.safetycity.controller.api;
 
 import com.Bigdata.safetycity.model.Crime;
-import com.Bigdata.safetycity.model.datas.Cctv;
 import com.Bigdata.safetycity.service.CrimeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -29,5 +25,16 @@ public class CrimeController {
     @GetMapping("api/crime/name/{name}")
     public ResponseEntity<List<Crime>> getCrimeByName(@PathVariable String name){
         return new ResponseEntity<>(crimeService.getCrimeByName(name), HttpStatus.OK);
+    }
+
+    @GetMapping("api/crime")
+    public ResponseEntity<List<List<Crime>>> getCrime(){
+        String[] crimeList = {"살인기수", "살인미수등", "강도", "강간", "유사강간", "기타 강간·강제추행등", "체포감금", "약취유인"};
+        ArrayList<List<Crime>> ret = new ArrayList<>();
+        for(String crime : crimeList){
+            List<Crime> arr = crimeService.getCrimeByName(crime);
+            ret.add(arr);
+        }
+        return new ResponseEntity<>(ret, HttpStatus.OK);
     }
 }
--- src/main/resources/static/demo/chart-area-demo.js
+++ src/main/resources/static/demo/chart-area-demo.js
@@ -3,24 +3,52 @@ Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSyste
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
 // Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["00:00~02:59", "03:00~05:59", "06:00~08:59", "09:00~11:59", "12:00~14:59", "15:00~17:59", "18:00~20:59", "21:00~23:59", "미상"],
-    datasets: [{
-      label: "Sessions",
-      lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
-      pointHitRadius: 50,
-      pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
-    }],
-  }
-});
+let myAreachart = document.getElementById("myAreaChart");
+let currtime = document.querySelector('.curr-time');
+
+let currentTime = new Date(Date.now()).fo
+currtime.innerHTML = currentTime.toString();
+
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+fetch("/api/crime")
+    .then((res) => res.json())
+    .then((datas) => {
+        let dataset = []
+
+        datas.map((data) => {
+            let randColor = random_rgba();
+            let obj = {
+                label: data[0].name,
+                headings: data[0].name,
+                fill: false,
+                lineTension: 0.3,
+                borderColor: randColor,
+                pointRadius: 5,
+                pointBackgroundColor: randColor,
+                pointBorderColor: "rgba(255,255,255,0.8)",
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: randColor,
+                pointHitRadius: 50,
+                pointBorderWidth: 2,
+                data: []
+            };
+            for ( let i = 0; i < data.length; i++ ) {
+                obj.data.push(data[i].count);
+            }
+            dataset.push(obj);
+      })
+        new Chart(myAreachart, {
+            type: 'line',
+            data: {
+                labels: ["00:00~02:59", "03:00~05:59", "06:00~08:59", "09:00~11:59", "12:00~14:59", "15:00~17:59", "18:00~20:59", "21:00~23:59"],
+                datasets: dataset,
+            },
+        });
+    });
+
+
+
