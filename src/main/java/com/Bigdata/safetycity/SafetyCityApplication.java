625d8514b325eaabf1581a454961e378c717f5d5
SangHyup <jsh170303@naver.com>
2022-01-03 10:20:22 +0900

    태블로 추가, 파일 업로드

--- src/main/java/com/Bigdata/safetycity/SafetyCityApplication.java
+++ src/main/java/com/Bigdata/safetycity/SafetyCityApplication.java
@@ -1,9 +1,12 @@
 package com.Bigdata.safetycity;
 
+import com.Bigdata.safetycity.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ FileStorageProperties.class })
 public class SafetyCityApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/Bigdata/safetycity/config/FileStorageProperties.java
@@ -0,0 +1,12 @@
+package com.Bigdata.safetycity.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "file")
+public class FileStorageProperties {
+    private String uploadDir;
+}
